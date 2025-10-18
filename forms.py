bcae12ef6bffce900c1e06925a6b468b05dd9c74
Lucky  Odi <luckyodi310@gmail.com>
2025-10-18 13:06:30 +0100

    Differentiate user and company profile form

--- forms.py
+++ forms.py
@@ -1,12 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms.fields.simple import StringField, SubmitField
+from wtforms.fields.simple import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
-class CompleteProfile(FlaskForm):
+class CompleteCompanyProfile(FlaskForm):
     full_name = StringField("Full Name", validators=[DataRequired()])
     location = StringField("Location", validators=[DataRequired()])
     company_name = StringField("Company Name", validators=[DataRequired()])
-    skills = StringField("Skills", validators=[DataRequired()])
-    bio = StringField("Bio", validators=[DataRequired()])
+    bio = TextAreaField("Company Profile", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class CompleteUserProfile(FlaskForm):
+    full_name = StringField("Full Name", validators=[DataRequired()])
+    location = StringField("Location", validators=[DataRequired()])
+    company_name = StringField("Last Company worked", validators=[DataRequired()])
+    skills = StringField("Skills e.g (React, AWS, Python, Docker, Java)", validators=[DataRequired()])
+    bio = TextAreaField("About Me", validators=[DataRequired()])
     experience_years = StringField("Years of Experience", validators=[DataRequired()])
+    degree = StringField("Degree Certificate e.g (B.Sc. Computer Science)", validators=[DataRequired()])
+    salary_range = StringField("Salary Exceptation e.g ($12k - $15k)", validators=[DataRequired()])
     submit = SubmitField("Submit")
